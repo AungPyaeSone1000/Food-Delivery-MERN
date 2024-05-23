addd9b0dda84c5f0702a317196ec533f5175b946
Aung Pyae Sone <ww.aps2002@gmail.com>
2024-05-23 14:32:15 -0400

    User Profile Form

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -6,5 +6,11 @@ import { validateMyUserRequest } from "../middleware/validation";
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 export default router;
